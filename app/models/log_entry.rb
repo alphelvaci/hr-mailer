60b6e6eaf8ae120e152cb4cf34aa7ce345be0bdf
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2023-07-20 14:39:47 +0300

    Add presence validations

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -1,4 +1,7 @@
 class LogEntry < ApplicationRecord
     belongs_to :recipient
     belongs_to :cc_recipient, class_name: "Recipient", optional: true
+
+    validates :status, presence: true
+    validates :recipient, presence: true
 end
--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,12 @@
 class Recipient < ApplicationRecord
     has_many :log_entries
     belongs_to :manager, class_name: "Recipient", optional: true
+
+    validates :kolay_ik_id, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :birth_date, presence: true
+    validates :employment_start_date, presence: true
+    validates :is_active, presence: true
 end
