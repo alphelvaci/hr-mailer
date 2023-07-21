5eea74151045409c65eb2baa0da54856ede3a497
Alp Helvaci <alp.helvaci@pisano.com>
2023-07-21 17:04:13 +0300

    Create CelebrationMailer and example templates

---
+++ app/mailers/celebration_mailer.rb
@@ -0,0 +1,19 @@
+class CelebrationMailer < ApplicationMailer
+    def birthday_email
+        @recipient = params[:recipient]
+        mail(
+            to: @recipient.email,
+            cc: @recipient.manager.blank? ? nil : @recipient.manager.email,
+            subject: "Happy Birthday #{@recipient.first_name} #{@recipient.last_name}!"
+        )
+    end
+    def work_anniversary_email
+        @recipient = params[:recipient]
+        @anniversary_year = DateTime.now.year - @recipient.employment_start_date.year
+        mail(
+            to: @recipient.email,
+            cc: @recipient.manager.blank? ? nil : @recipient.manager.email,
+            subject: "Happy Work Anniversary #{@recipient.first_name} #{@recipient.last_name}!"
+        )
+    end
+end
---
+++ test/mailers/previews/celebration_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/celebration_mailer
+class CelebrationMailerPreview < ActionMailer::Preview
+    def birthday_email
+        CelebrationMailer.with(recipient: Recipient.last).birthday_email
+    end
+    def work_anniversary_email
+        CelebrationMailer.with(recipient: Recipient.first).work_anniversary_email
+    end
+end
