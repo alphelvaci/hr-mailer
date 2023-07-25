6367d8aeb9f6da2f1c6d3bb9f5c6006adf7d9abe
Alp Helvaci <alp.helvaci@pisano.com>
2023-07-25 19:14:31 +0300

    Add log date helper

---
+++ app/helpers/logs_helper.rb
@@ -0,0 +1,12 @@
+module LogsHelper
+    def distance_to_date_in_words date
+        diff_in_minutes = ((Time.current() - date.at_beginning_of_day()) / 60).round
+        if diff_in_minutes.abs() <= 1440
+            return 'Today'
+        elsif diff_in_minutes < 0
+            return 'in ' + distance_of_time_in_words_to_now(date)
+        else
+            return distance_of_time_in_words_to_now(date) + ' ago'
+        end
+    end
+end
