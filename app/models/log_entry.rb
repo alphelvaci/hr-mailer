74b8cfd8f5eecc7c5bb1319558af70256b2f75a4
Alp Helvaci <alp.helvaci@pisano.com>
2023-07-27 16:58:02 +0300

    Add method to generate future logs

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -6,4 +6,52 @@ class LogEntry < ApplicationRecord
     validates :date, presence: true
     validates :status, presence: true
     validates :recipient, presence: true
+
+    def self.generate_future_logs
+        # TODO simplify this function
+
+        birthday_recipients =  Recipient.active.where([
+            "CAST(strftime('%m', birth_date) AS INTEGER) = ? AND CAST(strftime('%d', birth_date) AS INTEGER) >= ?",
+            Time.now.month,
+            Time.now.day
+        ]).or(Recipient.where([
+            "CAST(strftime('%m', birth_date) AS INTEGER) = ? AND CAST(strftime('%d', birth_date) AS INTEGER) <= ?",
+            Time.now.month + 1,
+            Time.now.day
+        ]))
+
+        for recipient in birthday_recipients do
+            LogEntry.find_or_create_by(
+                reason: 'birthday',
+                date: recipient.birth_date.change(year: 2023),
+                recipient: recipient,
+            ) do |new_log_entry|
+                new_log_entry.status = 'pending'
+                new_log_entry.cc_recipient = recipient.manager
+            end
+        end
+
+        work_anniversary_recipients =  Recipient.active.where([
+            "CAST(strftime('%m', employment_start_date) AS INTEGER) = ? AND CAST(strftime('%d', employment_start_date) AS INTEGER) >= ?",
+            Time.now.month,
+            Time.now.day
+        ]).or(Recipient.where([
+            "CAST(strftime('%m', employment_start_date) AS INTEGER) = ? AND CAST(strftime('%d', employment_start_date) AS INTEGER) <= ?",
+            Time.now.month + 1,
+            Time.now.day
+        ]))
+
+        for recipient in work_anniversary_recipients do
+            LogEntry.find_or_create_by(
+                reason: 'work_anniversary',
+                date: recipient.employment_start_date.change(year: 2023),
+                recipient: recipient,
+            ) do |new_log_entry|
+                new_log_entry.status = 'pending'
+                new_log_entry.cc_recipient = recipient.manager
+            end
+        end
+
+        return true
+    end
 end
