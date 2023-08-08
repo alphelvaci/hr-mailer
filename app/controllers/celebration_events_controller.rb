851167fe25e14098ec566e624c56e90e4c81497d
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-08 13:23:50 +0300

    Fix tomorrows event showing as past

--- app/controllers/celebration_events_controller.rb
+++ app/controllers/celebration_events_controller.rb
@@ -1,7 +1,7 @@
 class CelebrationEventsController < ApplicationController
   def index
     @future_celebration_events = CelebrationEvent.order(date: :desc).where(date: (Time.now.at_beginning_of_day + 1.day)..)
-    @celebration_events = CelebrationEvent.order(date: :desc).where(date: ..(Time.now.at_beginning_of_day + 1.day))
+    @celebration_events = CelebrationEvent.order(date: :desc).where(date: ..(Time.now.at_beginning_of_day))
   end
 
   def retry
