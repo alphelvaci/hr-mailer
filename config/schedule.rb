fa4a33eb0608b9276db9291f93d3204b9db8ed7e
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-15 11:34:19 +0300

    Job scheduling with whenever

---
+++ config/schedule.rb
@@ -0,0 +1,18 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Learn more: http://github.com/javan/whenever
+
+every 1.day, at: '4:00 am' do
+  runner 'DailyEventGenerationJob.perform_later'
+end
+
+every 1.day, at: '9:00 am' do
+  runner 'DailyCelebrationJob.perform_later'
+end
+
+every :monday, at: '6:00 am' do
+  runner 'WeeklyRecipientRefreshJob.perform_later'
+end
