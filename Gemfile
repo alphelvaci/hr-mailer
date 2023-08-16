f99be6c620befeed8dfae25aa3b6d2089bfe1492
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-16 14:02:22 +0300

    Add login functionality with devise gem

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem 'bootsnap', require: false
 # Use whenever to schedule jobs
 gem 'whenever', '~> 1.0'
 
+# Use devise for authentication
+gem "devise", "~> 4.9"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins, path_names: { sign_in: 'login', sign_out: 'logout' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '', to: redirect('celebration-events/')
   get 'celebration-events/', to: 'celebration_events#index'
   get 'celebration-events/:id/retry', to: 'celebration_events#retry'
 end
