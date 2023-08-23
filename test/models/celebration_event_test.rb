6046661c7d6fe3895b0361163a4e0a3e4bad23d4
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-23 17:04:54 +0300

    CelebrationEvent celebrate method tests

--- test/models/celebration_event_test.rb
+++ test/models/celebration_event_test.rb
@@ -5,3 +5,41 @@ class CelebrationEventTest < ActiveSupport::TestCase
     assert celebration_events(:one).pending?
   end
 
+  test 'celebrate method' do
+    celebration_events(:one).celebrate
+    assert_same 1, CelebrationMailer.deliveries.length
+    assert_same 1, CelebrationMailer.deliveries[0].to.length
+    assert_nil CelebrationMailer.deliveries[0].cc
+    assert_match recipients(:one).email, CelebrationMailer.deliveries[0].to[0]
+    assert_match 'Birthday', CelebrationMailer.deliveries[0].subject
+    assert celebration_events(:one).sent?
+
+    celebration_events(:two).celebrate
+    assert_same 2, CelebrationMailer.deliveries.length
+    assert_same 1, CelebrationMailer.deliveries[1].to.length
+    assert_same 1, CelebrationMailer.deliveries[1].cc.length
+    assert_match recipients(:two).email, CelebrationMailer.deliveries[1].to[0]
+    assert_match recipients(:two).manager.email, CelebrationMailer.deliveries[1].cc[0]
+    assert_match 'Anniversary', CelebrationMailer.deliveries[1].subject
+    assert celebration_events(:two).sent?
+
+    celebration_events(:three).celebrate(retry_: true)
+    assert_same 3, CelebrationMailer.deliveries.length
+    assert_same 1, CelebrationMailer.deliveries[2].to.length
+    assert_same 1, CelebrationMailer.deliveries[2].cc.length
+    assert_match recipients(:two).email, CelebrationMailer.deliveries[2].to[0]
+    assert_match recipients(:two).manager.email, CelebrationMailer.deliveries[2].cc[0]
+    assert_match 'Birthday', CelebrationMailer.deliveries[2].subject
+    assert celebration_events(:three).sent?
+    assert_nil celebration_events(:three).error_message
+
+    celebration_events(:four).celebrate(retry_: true)
+    assert_same 4, CelebrationMailer.deliveries.length
+    assert_same 1, CelebrationMailer.deliveries[3].to.length
+    assert_nil CelebrationMailer.deliveries[3].cc
+    assert_match recipients(:one).email, CelebrationMailer.deliveries[3].to[0]
+    assert_match 'Anniversary', CelebrationMailer.deliveries[3].subject
+    assert celebration_events(:four).sent?
+    assert_nil celebration_events(:four).error_message
+  end
+end
