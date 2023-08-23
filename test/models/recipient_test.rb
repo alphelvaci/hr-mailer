b00e296f45e8ab5718bf3e89d03a89fc18006246
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-23 15:59:55 +0300

    Recipient get_recipients_to_celebrate tests

--- test/models/recipient_test.rb
+++ test/models/recipient_test.rb
@@ -36,4 +36,23 @@ class RecipientTest < ActiveSupport::TestCase
       assert r.is_active
     end
   end
+
+  test 'get_recipients_to_celebrate method' do
+    # disallowed parameters days_ahead > 28 and reason not in ['birthday', 'work_anniversary']
+    assert_raises { Recipient.get_recipients_to_celebrate('birthday', 29) }
+    assert_raises { Recipient.get_recipients_to_celebrate('work_anniversary', 29) }
+    assert_raises { Recipient.get_recipients_to_celebrate('unknown', 29) }
+    assert_raises { Recipient.get_recipients_to_celebrate('unknown', 28) }
+
+    travel_to Date.new(2023, 12, 20) do
+      assert_same 3, Recipient.get_recipients_to_celebrate('birthday', 7).length
+      assert_same 2, Recipient.get_recipients_to_celebrate('work_anniversary', 7).length
+
+      assert_same 4, Recipient.get_recipients_to_celebrate('birthday', 28).length
+      assert_same 3, Recipient.get_recipients_to_celebrate('work_anniversary', 28).length
+
+      assert_not_includes Recipient.get_recipients_to_celebrate('birthday', 28), recipients(:inactive)
+      assert_not_includes Recipient.get_recipients_to_celebrate('work_anniversary', 28), recipients(:inactive)
+    end
+  end
 end
