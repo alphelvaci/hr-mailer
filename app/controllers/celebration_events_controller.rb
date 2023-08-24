6dde60e539cd3744d5426e90e08cca3872d4d737
Alp Helvaci <alp.helvaci@pisano.com>
2023-08-24 14:29:34 +0300

    Set a limit on displayed celebration events

--- app/controllers/celebration_events_controller.rb
+++ app/controllers/celebration_events_controller.rb
@@ -3,7 +3,9 @@ class CelebrationEventsController < ApplicationController
     @future_celebration_events = CelebrationEvent.order(date: :desc).where(
       date: (Time.now.at_beginning_of_day + 1.day)..
     )
-    @celebration_events = CelebrationEvent.order(date: :desc).where(date: ..(Time.now.at_beginning_of_day))
+    @celebration_events = CelebrationEvent.order(date: :desc).where(
+      date: (Time.now.at_beginning_of_day - 2.weeks)..(Time.now.at_beginning_of_day)
+    )
   end
 
   def retry
